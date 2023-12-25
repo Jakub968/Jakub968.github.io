f1bb51974c2908a150b0f9cc4c4218f1c02c4d22
Jakub Benický <jakubbenicky@gmail.com>
2023-12-25 16:12:18 +0100

    Add files via upload

--- script/scrollAnimation.js
+++ script/scrollAnimation.js
@@ -14,7 +14,9 @@ let boxPhotogalery = document.getElementsByClassName('box-photogalery')
 
 
 
-if (document.body.offsetWidth > 1024) {
+
+
+if (document.body.offsetWidth > 1024 && document.body.offsetWidth < 1280) {
     window.addEventListener('scroll', () =>{
         if (window.scrollY > 0) {
             line1[0].style.width = '70%'
@@ -28,7 +30,7 @@ if (document.body.offsetWidth > 1024) {
             servicesH1[0].style.opacity = '0'
             servicesH1[0].style.transform = 'translateX(-100px)'
         }
-        console.log(window.scrollY);
+        console.log(document.body.offsetWidth);
     
         if (window.scrollY > 342) {
             box[0].style.background = 'rgb(234, 234, 250)'
@@ -148,3 +150,128 @@ if (document.body.offsetWidth > 1024) {
 
 
 
+if (document.body.offsetWidth > 1280) {
+    window.addEventListener('scroll', () =>{
+        if (window.scrollY > 0) {
+            line1[0].style.width = '70%'
+    
+            servicesH1[0].style.opacity = '1'
+            servicesH1[0].style.transform = 'translateX(0px)'
+        } 
+        else{
+            line1[0].style.width = '0%'
+    
+            servicesH1[0].style.opacity = '0'
+            servicesH1[0].style.transform = 'translateX(-100px)'
+        }
+        console.log(window.scrollY);
+    
+        if (window.scrollY > 270) {
+            box[0].style.background = 'rgb(234, 234, 250)'
+            box[2].style.background = 'rgb(234, 234, 250)'
+    
+            box[0].children[0].style.opacity = '1'
+            box[0].children[0].style.transform = 'translateY(0px)'
+            box[0].children[1].style.opacity = '1'
+            box[0].children[1].style.transform = 'translateY(0px)'
+    
+            box[1].children[0].style.opacity = '1'
+            box[1].children[0].style.transform = 'translateY(0px)'
+            box[1].children[1].style.opacity = '1'
+            box[1].children[1].style.transform = 'translateY(0px)'
+    
+            box[2].children[0].style.opacity = '1'
+            box[2].children[0].style.transform = 'translateY(0px)'
+            box[2].children[1].style.opacity = '1'
+            box[2].children[1].style.transform = 'translateY(0px)'
+        }
+        else{
+            box[0].style.background = 'white'
+            box[2].style.background = 'white'
+    
+            box[0].children[0].style.opacity = '0'
+            box[0].children[0].style.transform = 'translateY(-100px)'
+            box[0].children[1].style.opacity = '0'
+            box[0].children[1].style.transform = 'translateY(100px)'
+    
+            box[1].children[0].style.opacity = '0'
+            box[1].children[0].style.transform = 'translateY(-100px)'
+            box[1].children[1].style.opacity = '0'
+            box[1].children[1].style.transform = 'translateY(100px)'
+    
+            box[2].children[0].style.opacity = '0'
+            box[2].children[0].style.transform = 'translateY(-100px)'
+            box[2].children[1].style.opacity = '0'
+            box[2].children[1].style.transform = 'translateY(100px)'
+    
+        }
+    
+        if (window.scrollY > (680)) {
+            line2[0].style.width = '70%'
+            aboutH1[0].style.transform = 'translateX(0px)'
+            aboutH1[0].style.opacity = '1'
+        }
+        else{
+            line2[0].style.width = '0%'
+            aboutH1[0].style.transform = 'translateX(-100px)'
+            aboutH1[0].style.opacity = '0'
+        }
+    
+    
+    
+    
+        if (window.scrollY > 910) {
+            boxAbout[0].children[0].style.transform = 'translateY(0px)'
+            boxAbout[0].children[0].style.opacity = '1'
+    
+            boxAbout[0].children[1].style.transform = 'translateY(0px)'
+            boxAbout[0].children[1].style.opacity = '1'
+        }
+        else{
+            boxAbout[0].children[0].style.transform = 'translateY(-100px)'
+            boxAbout[0].children[0].style.opacity = '0'
+    
+            boxAbout[0].children[1].style.transform = 'translateY(100px)'
+            boxAbout[0].children[1].style.opacity = '0'
+        }
+    
+    
+    
+        if (window.scrollY > 1200) {
+          technology.style.opacity = '1' 
+        }
+        else{
+            technology.style.opacity = '0' 
+        }
+    
+    
+    
+        if (window.scrollY > 1250) {
+            technologyCover[0].children[0].style.opacity = '1'
+            technologyCover[0].children[1].style.opacity = '1'
+    
+            technologyCover[0].children[0].style.transform = 'translateX(0px)'
+            technologyCover[0].children[1].style.transform = 'translateX(0px)'
+        }
+    
+        else {
+            technologyCover[0].children[0].style.opacity = '0'
+            technologyCover[0].children[1].style.opacity = '0'
+    
+            technologyCover[0].children[0].style.transform = 'translateX(100px)'
+            technologyCover[0].children[1].style.transform = 'translateX(-100px)'
+        }
+    
+    
+        if (window.scrollY > 1630) {
+            boxPhotogalery[0].children[0].style.opacity = '1'
+            boxPhotogalery[0].children[0].style.transform = 'translateX(0px)'
+            boxPhotogalery[0].children[1].style.width = '60%'
+        }
+        else{
+            boxPhotogalery[0].children[0].style.opacity = '0'
+            boxPhotogalery[0].children[0].style.transform = 'translateX(-100px)'
+            boxPhotogalery[0].children[1].style.width = '0%' 
+        }
+    })
+}
