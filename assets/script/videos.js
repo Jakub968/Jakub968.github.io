e72d79ed5132526dd7815e595a254bdb95989c1f
Jakub Benický <jakubbenicky@gmail.com>
2024-03-13 21:30:33 +0100

    Add files via upload

---
+++ assets/script/videos.js
@@ -0,0 +1,16 @@
+let video = document.getElementById('video')
+
+function video1() {
+    video.src = './assets/videos/KRIMI.mp4'
+    video.play();
+}
+
+function video2() {
+    video.src = './assets/videos/POUZITIE.mp4'
+    video.play();
+}
+
+function video3() {
+    video.src = './assets/videos/PREDSTAVENE.mp4'
+    video.play();
+}
