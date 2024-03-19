8294e4323f45577168140ac90dce7e83b2667fbb
Jakub Benický <jakubbenicky@gmail.com>
2024-03-19 16:59:28 +0100

    Add files via upload

--- assets/script/videos.js
+++ assets/script/videos.js
@@ -14,3 +14,21 @@ function video3() {
     video.src = './assets/videos/PREDSTAVENE.mp4'
     video.play();
 }
+
+function video4() {
+    video.src = './assets/videos/BLENDER.mp4'
+    video.play();
+}
+
+
+let playbackButton = document.getElementsByClassName('playback')
+
+for (let i = 0; i < 4; i++) {
+    playbackButton[i].addEventListener('mouseover', ()=>{
+        playbackButton[i].style.borderBottom = '30px solid white'
+    })
+
+    playbackButton[i].addEventListener('mouseout', ()=>{
+        playbackButton[i].style.borderBottom = ' 30px solid #e60c80'
+    })
+}
